38fd3e15f1c6ca31935ee7dd432d5bec7ef3a496
Kamalpreet Grewal <grewalkamal005@gmail.com>
2018-11-18 17:06:18 -0500

    Add basic layout for the game, board with numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This component renders a single button.
+class Square extends React.Component {
+	render() {
+		return (
+			<button className="square">
+			{this.props.value}
+			</button>
+		);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+	renderSquare(i) {
+		return <Square value={i}/>;
+	}
+
+
+	render() {
+		return (
+			<div>
+			<div className="board-row">
+				{this.renderSquare(0)}
+				{this.renderSquare(1)}
+				{this.renderSquare(2)}
+			</div>
+
+			<div className="board-row">
+				{this.renderSquare(3)}
+				{this.renderSquare(4)}
+				{this.renderSquare(5)}
+			</div>
+
+			<div className="board-row"></div>
+				{this.renderSquare(6)}
+				{this.renderSquare(7)}
+				{this.renderSquare(8)}
+			</div>
+		);
+	}
+}
+
+ReactDOM.render (
+	<Board />,
+	document.getElementById('root')
+);
